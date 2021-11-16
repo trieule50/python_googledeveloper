f8c734033a7bc7efcb687521b7728de7bba39f42
trieule50 <trieule50@gmail.com>
2021-11-16 11:30:12 -0500

    Complete verbing

--- basic_exercise/string2.py
+++ basic_exercise/string2.py
@@ -17,7 +17,13 @@
 # Return the resulting string.
 def verbing(s):
   # +++your code here+++
-  return
+  if len(s) >=3:
+    if s[-3:] == 'ing':
+      return s + 'ly'
+    else:
+      return s + 'ing'
+  else:
+    return s
 
 
 # E. not_bad
