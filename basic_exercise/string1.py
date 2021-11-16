b614fe75d3aea33de6322a20798d48113fd1386d
trieule50 <trieule50@gmail.com>
2021-11-16 11:22:52 -0500

    Completed mix up exercise

--- basic_exercise/string1.py
+++ basic_exercise/string1.py
@@ -69,7 +69,7 @@ def fix_start(s):
 # Assume a and b are length 2 or more.
 def mix_up(a, b):
   # +++your code here+++
-  return
+  return b[:2]+a[2:] +' '+ a[:2]+b[2:]
 
 
 # Provided simple test() function used in main() to print
