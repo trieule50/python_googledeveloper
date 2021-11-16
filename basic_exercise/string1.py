6df2b0da64338370dd976b8f7d9102569e9e6691
trieule50 <trieule50@gmail.com>
2021-11-16 10:57:11 -0500

    Complete fix start

--- basic_exercise/string1.py
+++ basic_exercise/string1.py
@@ -55,8 +55,9 @@ def both_ends(s):
 # where all instances of stra have been replaced by strb.
 def fix_start(s):
   # +++your code here+++
-
-  return
+  a = s[0]
+  b = s[1:]
+  return a + b.replace(a, "*")
 
 
 # D. MixUp
