b04f329f11314ed41cce3af3491b6453f158e573
trieule50 <trieule50@gmail.com>
2021-11-19 10:30:26 -0500

    Complete front_back

--- basic_exercise/string2.py
+++ basic_exercise/string2.py
@@ -51,7 +51,13 @@ def not_bad(s):
 #  a-front + b-front + a-back + b-back
 def front_back(a, b):
   # +++your code here+++
-  return
+  a_middle = len(a) / 2
+  b_middle = len(b) / 2
+  if len(a) % 2 == 1:  # add 1 if length is odd
+    a_middle = a_middle + 1
+  if len(b) % 2 == 1:
+    b_middle = b_middle + 1 
+  return a[:a_middle] + b[:b_middle] + a[a_middle:] + b[b_middle:]
 
 
 # Simple provided test() function used in main() to print
